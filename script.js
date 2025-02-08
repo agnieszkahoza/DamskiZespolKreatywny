0998f9f4cb14fe1e43ca505299ebc85549837d3f
julaarchitek <juliaketihcra@gmail.com>
2025-02-08 15:39:09 +0100

    changes in css

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-import "./css/style.css";
 
-const $h3 = document.getElementById("author");
-
-$h3.addEventListener("mouseover", function (e) {
-  e.target.classList.add("redTitle");
-});
-
-$h3.addEventListener("mouseleft", function (e) {
-  e.target.classList.remove("redTitle");
-});
